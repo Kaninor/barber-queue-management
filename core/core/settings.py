f7fb200cf375a0f4d8f08deb9cd76bf9b0d5fc4e
unknown <dariushroouhifard@gmail.com>
2022-07-02 12:44:18 +0430

    working

--- core/core/settings.py
+++ core/core/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'barbers',
     'personnels',
+    'generals'
 ]
 
 MIDDLEWARE = [
--- core/core/urls.py
+++ core/core/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('barbers.urls')),
-    path('', include('personnels.urls'))
+    path('', include('personnels.urls')),
+    path('', include('generals.urls')),
 ]
